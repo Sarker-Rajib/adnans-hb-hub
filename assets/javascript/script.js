6f6781e59aa568c8b7bfeff01069580833f7ed96
Rajib Sarker <redhat0593@gmail.com>
2023-06-10 14:13:32 +0600

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,63 @@
+// world clock slider
+const clockSwiper = new Swiper(".clockSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 5,
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 0,
+        },
+        1200: {
+            slidesPerView: 8,
+            spaceBetween: 0,
+        },
+        1396: {
+            slidesPerView: 10,
+            spaceBetween: 0,
+        },
+    },
+    navigation: {
+        nextEl: ".world-clock-next",
+        prevEl: ".world-clock-prev",
+    },
+});
+
+// nav-tab slider
+const navTab = new Swiper(".navTabSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        }
+    },
+    navigation: {
+        nextEl: ".nav-tab-next",
+        prevEl: ".nav-tab-prev",
+    },
+});
+
+
+// Banner slider
+const bannerSwiper = new Swiper(".bannerSwiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
